64283a9a9f0888eba3f82ca819ed587ba4a7c5f3
Sana Zaidi <sana.rizvi.5@gmail.com>
2019-11-24 14:50:09 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@ def key_for_min_value(flowers)
   lowest_key = nil 
   lowest_value = nil 
   flowers.each do |key, value| 
-    if lowest_value = nil or value < lowest_value
+    if lowest_value == nil or value < lowest_value
       lowest_value = value 
       lowest_key = key 
     end 
